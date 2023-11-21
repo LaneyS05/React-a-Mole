075fd54587056944e0ed2bfbe74a13f3fd6a05cc
LaneyS05 <laney.staggs05@gmail.com>
2023-11-21 13:51:04 -0500

    initial commit

---
+++ src/Mole.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import moleImg from "./mole.png";
+
+function Mole(props) {
+  useEffect(() => {
+    let randSeconds = Math.ceil(Math.random() * 10000);
+    let timer = setTimeout(() => {
+      // instructions said
+      props.toggle(false);
+    }, randSeconds);
+    return () => clearTimeout(timer);
+  });
+
+  return (
+    <div>
+      <img
+        style={{ width: "30vw" }}
+        src={moleImg}
+        alt="Mole"
+        onClick={props.handleClick}
+      />
+    </div>
+  );
+}
+
+export default Mole;
